a8cdc94e510571f6f8739d46da9f6baae62db5fb
Vokinneberg <evgeniy81@gmail.com>
2020-12-06 23:36:31 -0500

    Add initial cache implementation

---
+++ cache.go
@@ -0,0 +1,79 @@
+package negroni_cache
+
+import (
+	"hash/fnv"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+
+	lru "github.com/hashicorp/golang-lru"
+	"github.com/urfave/negroni/v2"
+)
+
+var cachableVerbs = map[string]interface{} {
+	http.MethodGet: nil,
+}
+
+type HttpResponseEntry struct {
+	Status int
+	Header http.Header
+	Body   []byte
+}
+
+type HttpCache struct {
+	cache *lru.Cache
+}
+
+func NewHttpCache(size int) *HttpCache {
+	c, err := lru.New(size)
+	if err != nil {
+		panic("unable to initialize cache")
+	}
+	return &HttpCache{cache: c}
+}
+
+func (c *HttpCache) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	key := cacheKey(r.URL)
+	if _, ok := cachableVerbs[r.Method]; ok {
+		if c.cache.Contains(key) {
+			if respEntry, ok := c.cache.Get(key); ok {
+				resp := respEntry.(HttpResponseEntry)
+				for k, v := range resp.Header {
+					rw.Header().Set(k, strings.Join(v, ";"))
+				}
+				rw.WriteHeader(resp.Status)
+				_, err := rw.Write(resp.Body)
+				if err != nil {
+					panic(err)
+				}
+				return
+			}
+		}
+
+		w := httptest.NewRecorder()
+		crw := newCacheResponseWriter(rw.(negroni.ResponseWriter), w)
+
+		next(crw, r)
+
+		c.cache.Add(key, HttpResponseEntry{Status: w.Code, Header: w.Result().Header, Body: w.Body.Bytes()})
+
+		rw.WriteHeader(w.Code)
+		_, err := rw.Write(w.Body.Bytes())
+		if err != nil {
+			panic(err)
+		}
+	}
+	next(rw, r)
+}
+
+func cacheKey(u *url.URL) string {
+	hash := fnv.New128a()
+	_, err := hash.Write([]byte(u.String()))
+	if err != nil {
+		panic(err)
+	}
+
+	return string(hash.Sum(nil))
+}
+
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vokinneberg/negroni-cache
+
+go 1.15
+
+require github.com/urfave/negroni/v2 v2.0.2
+
+require (
+	github.com/hashicorp/golang-lru v0.5.4
+)
